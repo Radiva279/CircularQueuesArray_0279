cf562b7ed7c38e1578a06bb399f48f18dbe87777
Radiva279 <radivagalih12@gmail.com>
2025-06-13 09:31:58 +0700

    memanggil prosedur

--- circularQueuess.cpp
+++ circularQueuess.cpp
@@ -133,3 +133,35 @@ public:
                 cin >> ch;
                 cout << endl;
 
+                switch (ch)
+                {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+                case '4': {
+                    return 0;
+                }
+                default: {
+                    cout << "Invlaid option!!" << endl;
+                    break;
+                }
+                }
+            }
+            catch(exception& e) {
+                cout << "Check for the values entered." << endl;
+            }
+        }
+    }
+};
