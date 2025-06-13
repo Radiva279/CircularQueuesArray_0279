1e463d6d4b2ae10a38b230e0edb280b6dc391bca
Radiva279 <radivagalih12@gmail.com>
2025-06-13 09:19:00 +0700

    front > rear = menandai queue sudah circular array

--- circularQueuess.cpp
+++ circularQueuess.cpp
@@ -90,3 +90,12 @@ public:
             }
             cout << endl;
         }
+        else {
+            //Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir
+            while(FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
