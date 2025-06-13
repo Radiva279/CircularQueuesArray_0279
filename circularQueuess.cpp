69e9c531e80f6af43625ecd38fc618580a6f9a2c
Radiva279 <radivagalih12@gmail.com>
2025-06-13 09:31:22 +0700

    membuat tampilan menu

--- circularQueuess.cpp
+++ circularQueuess.cpp
@@ -70,41 +70,66 @@ public:
         }
     }
 
-    void display(){
+    void display()
+    {
         int FRONT_position = FRONT;
         int REAR_position = REAR;
 
-        //cek apakah antrian kosong
-        if (FRONT == -1){
-            cout << "Queue is empty\n" ;
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
             return;
         }
 
         cout << "\nElements in the queue are...\n";
 
-        //Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
-        if (FRONT_position <= REAR_position) {
-            while (FRONT_position <= REAR_position) {
-             cout << queue_array{FRONT_position} << "   ";
-             FRONT_position++;   
+        // Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array{FRONT_position} << "   ";
+                FRONT_position++;
             }
             cout << endl;
         }
-        else {
-            //Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir
-            while(FRONT_position <= max - 1) {
+        else
+        {
+            // Jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir
+            while (FRONT_position <= max - 1)
+            {
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
             }
 
             FRONT_position = 0;
 
-            //Iterasi dari awal array hingga REAR
-            while (FRONT_position <= REAR_position) {
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
             }
             cout << endl;
         }
     }
+
+    int main()
+    {
+        Queues q;
+        char ch;
+
+        while (true)
+        {
+            try
+            {
+                cout << "Menu" << endl;
+                cout << "1. Implement insert operation" << endl;
+                cout << "2. Implement delete operation" << endl;
+                cout << "3. Display values" << endl;
+                cout << "4. Exit" << endl;
+                cout << "Enter your choice (1-4): ";
+                cin >> ch;
+                cout << endl;
 
