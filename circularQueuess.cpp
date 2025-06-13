ebfe8b8d81c6a13c0446950bf90c9da84231c397
Radiva279 <radivagalih12@gmail.com>
2025-06-13 08:07:58 +0700

    membuat prosedur insert

--- circularQueuess.cpp
+++ circularQueuess.cpp
@@ -1,13 +1,22 @@
-#include  <iostream>
+#include <iostream>
 using namespace std;
 
-class Queues {
+class Queues
+{
     int FRONT, REAR, max = 5;
     int queue_array[5];
 
-    public:
-        Queues(){
-            FRONT = -1;
-            REAR = -1;
-        }
-};
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
